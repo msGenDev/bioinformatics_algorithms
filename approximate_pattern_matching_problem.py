a52729e205d3b1d01d42148eee743172eaf5c43b
noelnamai <noelnamai@fas.harvard.edu>
2014-06-01 18:31:50 -0400

    Update

--- approximate_pattern_matching_problem.py
+++ approximate_pattern_matching_problem.py
@@ -1,27 +1,23 @@
 ## Python function to find all starting positions where Pattern appears as a substring of Text with at most d mismatches
 ## given strings Pattern and Text along with an integer d.
 
-def approximate_pattern_matching_problem(pattern, text , d):
-        
-    the_index = []
-    
-    for index in range(len(text)):
-        partition = text[index: index+len(pattern)]
-        
-        if len(partition) == len(pattern): 
-            matches = 0
-            mismatches = 0
-        
-            for i in range(len(pattern)):
-                if pattern[i] == partition[i]:
-                    matches += 1                        
-                else:
-                    mismatches += 1
-                    
-            if mismatches <= d:
-                the_index.append(index)
-                
-    return " ".join(map(str, the_index))
+def find_pattern(p, q, d):
+    count = 0
+    for x, y in zip(p,q):
+        if x != y: 
+            count = count + 1
+        if count > d:
+            return False
+    return True
+	
+def approximate_pattern_matching_problem(pattern, genome, d):
+    pos = []
+    k = len(pattern)
+    l = len(genome)
+    for i in range(l-k):
+        if appmatchpat(pattern, genome[i:i+k], d):
+            pos = pos + [i]
+    return pos
     
 string =  "".join(open("approximate_match_data.txt")).split()
 approximate_pattern_matching_problem(string[0], string[1], int(string[2]))
--- clump_finding_problem.py
+++ clump_finding_problem.py
@@ -1,34 +1,36 @@
 ## Python function to find all distinct k-mers forming (L, t)-clumps in Genome given string Genome, and integers k, L, and t.
 
+import itertools
 from collections import Counter
 
 def frequent_words_problem(string, k):
 
     words = []
-    results = []    
-    
+    results = []      
+	
     for i in range(len(string)):  
         word = "".join(string[i: i+k])
+		
         if len(word) == k:
-            words.append(word)
-                
+            words.append(word)  
+			
     return Counter(words).most_common()
 
 #string = "".join(open('frequent_words_problem_text.txt')).split()
 #frequent_words_problem(string[0], int(string[1]))
 
-import itertools
-
-def clump_finding_problem(string, k, L, t):
-    
-    words = []
-    
+def clump_finding_problem(string, k, L, t):  
+  
+    words = []    
+	
     for i in range(len(string)):
         string_1 = string[i: i+L]
+		
         if len(string_1) == L:            
             words.append(frequent_words_problem(string_1, k))
             
     dummy = list(itertools.chain(*words))
+	
     return [y for y in set([x[0] for x in dummy if x[1] >= t])]
             
 k = 11
